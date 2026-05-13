9ba2c195009553bcb2cd8dbde1106b68cd563fd5
Sole <soledadmoral13@gmail.com>
2026-05-13 18:32:38 +0200

    fix error de str en vez de int

--- src/sale.py
+++ src/sale.py
@@ -1,6 +1,6 @@
 class Sale:
     def __init__(self, sale_id, client_id, product, category, amount, date):
-        self.sale_id = int(sale_id)
+        self.sale_id = str(sale_id)
         self.client_id = int(client_id)
         self.product = str(product)
         self.category = str(category)
